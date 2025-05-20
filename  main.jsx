ea8b96c6e7d92df04dedae54037474858018418b
ChooPersonal <kokhwa.choo@gmail.com>
2025-05-20 18:17:22 +0800

    Delete  main.jsx

---  main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import RosteringMVP from "./RosteringMVP";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RosteringMVP />
-  </React.StrictMode>
-);
