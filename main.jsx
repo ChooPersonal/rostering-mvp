e0e147aed20f9c277d18728e35635cc3d7bf7c58
ChooPersonal <kokhwa.choo@gmail.com>
2025-05-20 18:16:04 +0800

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import RosteringMVP from "./RosteringMVP";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RosteringMVP />
+  </React.StrictMode>
+);
